0e4edf9d58b70df6848a09ccca709a474a538d27
jonasschultheiss <jonas.raphael.schultheiss@gmail.com>
2023-04-05 15:18:26 +0200

    feat: disabled dynamic roles

--- src/bot/bot.module.ts
+++ src/bot/bot.module.ts
@@ -2,11 +2,10 @@ import { DiscordModule } from '@discord-nestjs/core';
 import { Module } from '@nestjs/common';
 import { BotGateway } from './bot.gateway';
 import { ChannelsModule } from './channels/channels.module';
-import { DynamicRolesModule } from './dynamic-roles/dynamic-roles.module';
 import { EventsModule } from './events/events.module';
 
 @Module({
-  imports: [DiscordModule.forFeature(), DynamicRolesModule, EventsModule, ChannelsModule],
+  imports: [DiscordModule.forFeature(), EventsModule, ChannelsModule],
   providers: [BotGateway]
 })
 export class BotModule {}
